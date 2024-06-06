fc1cc6acb916d48524a87f75f80b6ef596e18bee
SeongJun08 <ellajune1612@gmail.com>
2024-06-06 15:08:18 +0900

    선언 정의 분리 및 정리

--- TowerOfHanoi.cpp
+++ TowerOfHanoi.cpp
@@ -1,18 +1,5 @@
 ﻿#include <iostream>
-
-void Hanoi(int n, char from, char temp, char to)
-{
-    if (n == 1)
-    {
-        std::cout << "Move " << from << " -> " << to << "로" << " 원반 " << n << "을 옮깁니다" << std::endl;
-    }
-    else
-    {
-        Hanoi(n - 1, from, to, temp);
-        std::cout << "Move " << from << " -> " << to << "로" << " 원반 " << n << "을 옮깁니다" << std::endl;
-        Hanoi(n - 1, temp, from, to);
-    }
-}
+#include "hanoi.h"
 
 int main()
 {
@@ -23,13 +10,14 @@ int main()
     A B C의 원판이 있고 A 원판에 3개의 원반이 매달려있다
     원반은 아래에서부터 위로갈수록 작아진다
     원반은 자신보다 작은 원반 위에 놓을 수 없다
+    한번에 하나의 원판만 옮길 수 있다
     A에 쌓여있는 원반을 C로 옮기면 성공이다
     재귀를 써본 뒤 제한없이 만들어보자
     */
 
-    int wonPan;
-    std::cout << " 원판의 갯수를 입력하세요" << std::endl;
-    std::cin >> wonPan;
-    Hanoi(wonPan, 'A', 'B', 'C');
+    int n = InputFromUser();
+    Hanoi(n, 'A', 'B', 'C');
+    return 0;
+
 
 }
---
+++ hanoi.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <iostream>
+
+int InputFromUser();
+void Move(char from, char to);
+void Hanoi(int n, char from, char temp, char to);
